85154b66b012d43cbef1d9657aaf2553a2e1ca06
Rohit Kumar <rohitkumar377024@gmail.com>
2019-10-15 23:33:53 +0530

    Google Test Banner Ad Integrated

--- app/src/main/java/com/example/goodearning/nav/AnswerAndEarnFragment.kt
+++ app/src/main/java/com/example/goodearning/nav/AnswerAndEarnFragment.kt
@@ -6,12 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.goodearning.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
+import kotlinx.android.synthetic.main.fragment_answer_and_earn.*
 
 class AnswerAndEarnFragment : Fragment() {
 
+    private lateinit var adView: AdView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_answer_and_earn, container, false)
+        val v = inflater.inflate(R.layout.fragment_answer_and_earn, container, false)
+
+        adView = v.findViewById(R.id.adView)
+        setup()
+
+        return v // Inflate the layout for this fragment
+    }
+
+    private fun setup() {
+        /* Initializing Mobile Ads SDK */
+//        MobileAds.initialize(context, getString(R.string.admob_app_id))
+
+        MobileAds.initialize(context, getString(R.string.admob_app_id))
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
 }
